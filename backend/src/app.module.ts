ac8cf1cb0709a39480e9f7a786f5bb23c73153c7
Hyro32 <mcorrochanoarroyo@gmail.com>
2024-09-13 21:54:03 +0200

    @nestjs/config import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
